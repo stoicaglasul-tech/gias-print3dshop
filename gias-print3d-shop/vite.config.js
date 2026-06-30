9927cbfce4bbdb8293791c66b9d229ecce8a0084
Gia Stoica <stoica.glasul@gmail.com>
2026-06-30 05:54:57 +0300

    Fix base path for new repo name

--- gias-print3d-shop/vite.config.js
+++ gias-print3d-shop/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-react-project/', // 👈 MAKE SURE THIS IS EXACTLY YOUR REPO NAME
+  base: '/gias-print3dshop/', // 👈 MAKE SURE THIS IS EXACTLY YOUR REPO NAME
 })
